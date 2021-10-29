805b49e16ddb9a1c6e3433954205d299ebfc256f
Sayujya Malkan <sayujyamalkan@gmail.com>
2021-10-29 19:39:33 +0530

    final commit(1)

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -34,7 +34,7 @@ function Banner() {
       <Container className={classes.bannerContent}>
         <div className={classes.tagline}>
           <Typography
-            variant="h2"
+            variant="h3"
             style={{
               fontWeight: "bold",
               marginBottom: 15,
